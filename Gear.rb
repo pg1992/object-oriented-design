f93428c16afde6d345da4fa67d4d78b74f63fb3d
Pedro Guilherme S. Moreira <pedro.eletronica.unb@gmail.com>
2018-01-25 14:06:30 -0200

    Print the `gear inches` of the bicycle
    Bicyclists uses `gear inches` as a more meaningful metric for bicycles
    that differ both in gearing and wheel size.

--- Gear.rb
+++ Gear.rb
@@ -17,5 +17,5 @@ class Gear
     end
 end
 
-puts Gear.new(52, 11, 26, 1.5).ratio
-puts Gear.new(30, 27, 24, 1.25).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(30, 27, 24, 1.25).gear_inches
